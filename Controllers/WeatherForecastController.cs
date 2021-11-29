a6d3d2af26c44822d096138545a022154280dc54
Michał Domański <m.domanski.hobosystems@gmail.com>
2021-11-29 13:42:23 +0100

    xxx

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace HMS.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "aaaaa"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
